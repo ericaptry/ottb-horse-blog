19ed840a84d6099d95d26126e36037620d8718d4
ericaptry”git config --global user.email erica.patry05@gmail.com” <erica.patry05@gmail.com>
2016-11-21 15:41:32 +0000

    images to show in prod, heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,25 +10,25 @@ helper_method :current_user, :logged_in?
 
 def current_user
 
-@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
 
 end
 
 def logged_in?
 
-!!current_user
+  !!current_user
 
 end
 
 def require_user
 
-if !logged_in?
+  if !logged_in?
 
-flash[:danger] = "You must be logged in to perform that action"
+    flash[:danger] = "You must be logged in to perform that action"
 
-redirect_to root_path
+    redirect_to root_path
 
-end
+  end
 
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,31 +6,31 @@ before_action :require_same_user, only: [:edit, :update]
 
 def index
 
-@users = User.paginate(page: params[:page], per_page: 5)
+  @users = User.paginate(page: params[:page], per_page: 5)
 
 end
 
 def new
 
-@user = User.new
+  @user = User.new
 
 end
 
 def create
 
-@user = User.new(user_params)
+  @user = User.new(user_params)
 
-if @user.save
+    if @user.save
 
-flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
 
-redirect_to articles_path
+      redirect_to articles_path
 
-else
+    else
 
-render 'new'
+       render 'new'
 
-end
+    end
 
 end
 
@@ -40,51 +40,51 @@ end
 
 def update
 
-if @user.update(user_params)
+  if @user.update(user_params)
 
-flash[:success] = "Your account was updated successfully"
+      flash[:success] = "Your account was updated successfully"
 
-redirect_to articles_path
+      redirect_to articles_path
 
-else
+  else
 
-render 'edit'
+      render 'edit'
 
-end
+  end
 
 end
 
 def show
 
-@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
 end
 
 private
 
-def user_params
+  def user_params
 
-params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
 
-end
+  end
 
-def set_user
+  def set_user
 
-@user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-end
+  end
 
-def require_same_user
+  def require_same_user
 
-if current_user != @user
+    if current_user != @user
 
-flash[:danger] = "You can only edit your own account"
+      flash[:danger] = "You can only edit your own account"
 
-redirect_to root_path
+      redirect_to root_path
 
-end
+    end
 
-end
+  end
 
 end
 
